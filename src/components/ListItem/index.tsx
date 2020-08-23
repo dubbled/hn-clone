cfed45004341a39c173222831f26d4c7f49f825a
dubbled <asaknowsbest@gmail.com>
2020-08-23 11:19:14 -0500

    removed unecessary component

--- src/components/ListItem/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-import { Card, ListGroup } from 'react-bootstrap'
-export default (props) => (
-  <ListGroup.Item key={props.story.url}>
-    <h6 style={{textAlign: 'left'}}>{props.story.title}</h6>
-  </ListGroup.Item>
-)
