fe53c6ad22d874645e3def3623ed32e4cb983905
dubbled <asaknowsbest@gmail.com>
2020-08-23 11:10:37 -0500

    Updates

---
+++ precache-manifest.3e2b260d0f1fa100d436f3f986b15108.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6e949f926582753ae366ef7ec906573",
+    "url": "/hn-clone/index.html"
+  },
+  {
+    "revision": "cde5dd1c10bedbeac07d",
+    "url": "/hn-clone/static/css/main.00838fcf.chunk.css"
+  },
+  {
+    "revision": "3c180555e4bc7dcf53b6",
+    "url": "/hn-clone/static/js/2.69089e6a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/hn-clone/static/js/2.69089e6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cde5dd1c10bedbeac07d",
+    "url": "/hn-clone/static/js/main.86590f4d.chunk.js"
+  },
+  {
+    "revision": "cb6aaa7f251b8d06fc56",
+    "url": "/hn-clone/static/js/runtime-main.d75da736.js"
+  }
+]);
